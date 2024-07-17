8ab82971649d1ddd17b64ea1133de288caed383a
Tamzid Israk <tamzidisrak12@gmail.com>
2024-07-17 15:36:32 +0600

    Added Service Registry

---
+++ ServiceRegistery/src/main/java/com/example/ServiceRegistery/ServiceRegistery/ServiceRegisteryApplication.java
@@ -0,0 +1,14 @@
+package com.example.ServiceRegistery.ServiceRegistery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegisteryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryApplication.class, args);
+	}
+
+}
