c681bbb262943ab6ffdc9c22374596f2f3b81b96
Tamzid Israk <tamzidisrak12@gmail.com>
2024-07-29 06:52:51 +0600

    Register Rating Service and Hotel Service to the Eureka Server

--- HotelService/src/main/java/com/microservice/hotel/HotelService/HotelServiceApplication.java
+++ HotelService/src/main/java/com/microservice/hotel/HotelService/HotelServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.hotel.HotelService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
--- hotelRating/src/main/java/com/example/rating/hotelRating/HotelRatingApplication.java
+++ hotelRating/src/main/java/com/example/rating/hotelRating/HotelRatingApplication.java
@@ -2,8 +2,10 @@ package com.example.rating.hotelRating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HotelRatingApplication {
 
 	public static void main(String[] args) {
