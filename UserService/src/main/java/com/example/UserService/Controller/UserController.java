82ba14ad71179e6714900383d3f108b3fa25bc3f
Tamzid Israk <tamzidisrak12@gmail.com>
2024-08-19 07:16:37 +0600

    1. Add Rate Limiter to avoid dos attack

--- UserService/src/main/java/com/example/UserService/Controller/UserController.java
+++ UserService/src/main/java/com/example/UserService/Controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.UserService.model.Rating;
 import com.example.UserService.model.User;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,8 @@ public class UserController {
 
     //create user
     @PostMapping
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+    @RateLimiter(name = "userRateLimiter",fallbackMethod = "ratingHotelFallbackMethod")
     public ResponseEntity<User> createUser(@RequestBody User user){
         User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
@@ -32,6 +35,8 @@ public class UserController {
 
     //get Single user by id
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+    @RateLimiter(name = "userRateLimiter",fallbackMethod = "ratingHotelFallbackMethod")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user = userService.getUserById(userId);
         return ResponseEntity.ok(user);
@@ -41,13 +46,15 @@ public class UserController {
     //get all User
     @GetMapping
     @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+    @RateLimiter(name = "userRateLimiter",fallbackMethod = "ratingHotelFallbackMethod")
     public ResponseEntity<List<User>> getAllUser(){
         List<User> userList = userService.getAllUser();
         return ResponseEntity.ok(userList);
     }
 
 
-    //Note: the error method returns the dummy user.
+    //Note: the error method returns the dummy user. 
+    //Note: Better to show output message or error message. example: Too many request or Service down
     public ResponseEntity<User> ratingHotelFallbackMethod(Exception exception){
         System.out.println("error Message : "+exception.getMessage());
         User errorUser = User.builder()
@@ -61,6 +68,8 @@ public class UserController {
 
 
     @PostMapping("/setRating/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+    @RateLimiter(name = "userRateLimiter",fallbackMethod = "ratingHotelFallbackMethod")
     public ResponseEntity<User> setRatingByUser(@PathVariable String userId,@RequestBody Rating rating){
         User user = userService.setRatingByUserId(userId,rating);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
