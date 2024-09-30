053148d13f17815be925d613b9cf0aa7527a5780
Tamzid Israk <tamzidisrak12@gmail.com>
2024-10-01 05:35:59 +0600

    add okta with api gateway

---
+++ Api-Gateway/src/main/java/com/example/apiGateway/Api_Gateway/Model/AuthResponse.java
@@ -0,0 +1,25 @@
+package com.example.apiGateway.Api_Gateway.Model;
+
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private Long expireAt;
+    private Collection<String> authorities;
+}
